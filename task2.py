758616552d9280b76b790c05b789b373a473de63
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-08-18 05:01:45 +0100

    create a function that prints my name

---
+++ task2.py
@@ -0,0 +1,10 @@
+# This program simp;y prints out my full name
+
+
+def say_my_name():
+    """This funtion simply returns the name variable"""
+    name = "Akinde Abdulmumin Opeyemi"
+    return name
+
+
+print(say_my_name())
